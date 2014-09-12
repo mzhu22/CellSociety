02e0a42201ae6ec67ba12f09f1f5bed1e18812e1
Brian Bolze <brianbolze@gmail.com>
2014-09-12 14:15:18 -0400

    First changes to main.java

--- src/cellsociety_team08/Main.java
+++ src/cellsociety_team08/Main.java
@@ -3,8 +3,12 @@ package cellsociety_team08;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		doBrianStuff();
 
+	}
+	
+	private static void doBrianStuff() {
+		System.out.println("I'm Brian!!!");
 	}
 
 }

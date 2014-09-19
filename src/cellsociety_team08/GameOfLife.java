4652683507654cf9d326fe1af66823c78282c290
jdc61 <jdc61@duke.edu>
2014-09-18 23:00:21 -0400

    game of life stuff

--- src/cellsociety_team08/GameOfLife.java
+++ src/cellsociety_team08/GameOfLife.java
@@ -1,21 +1,45 @@
 package cellsociety_team08;
 
+import java.util.List;
+
+import javafx.scene.paint.Color;
+
 public class GameOfLife extends RuleSet {
 
 	private static final String GAME_OF_LIFE = "Game of Life";
 
 	private static final State[] possibleStates = new State[] {
-			new State("Alive", 0, null) };
+			new State("Alive", 0, Color.GREEN, null) };
 
 	public GameOfLife(Object[] params) {
 		super(GAME_OF_LIFE, possibleStates, params);
 	}
 
 	@Override
-	public State getState(Cell[][] neighborhood) {
+	public Patch getNext(Patch patch, List<Patch> neighborhood) {
+		
+		if (patch.containsCell()) {
+			if (neighborhood.size() < 2) {
+				patch.myCell = null;
+				patch.clear();
+			}
+			if (neighborhood.size() > 3) {
+				patch.myCell = null;
+				patch.clear();
+			}
+		}
+		
+		if (!patch.containsCell()) {
+			if (neighborhood.size() == 3) {
+				Cell newCell = new Cell(possibleStates[0], patch.myDimensions);
+				patch.fill(newCell);
+			}
+		}
+		
+		return patch;
 
-		int count = 0;
-		Cell currCell = neighborhood[1][1];
+		/*int count = 0;
+		Patch currentPatch = neighborhood[1][1];
 
 		for (int i = 0; i < neighborhood.length; i++) {
 			for (int j = 0; j < neighborhood[0].length; j++) {
@@ -26,32 +50,31 @@ public class GameOfLife extends RuleSet {
 			}
 		}
 		
-		if (currCell.isEmpty && count == 3) {
-			revive(currCell);
+		if (currentPatch.isEmpty && count == 3) {
+			revive(currentPatch);
 			return possibleStates[0];
 		}
 
 		if (count < 2)
-			kill(currCell);
+			kill(currentPatch);
 		else if (count > 3)
-			kill(currCell);	
+			kill(currentPatch);	
 		else if (count == 2 || count == 3)
-			return currCell.getState();
+			return currentPatch.getNext();
 		
-		return null; // returns null if cell is newly empty
+		return null; // returns null if cell is newly emp
+		*/
 	}
 
-	private void kill(Cell cell) {
-		cell.isEmpty = true;
-	}
+
 	
-	private void revive(Cell cell) {
+	/*private void revive(Cell cell) {
 		cell.isEmpty = false;
-	}
+	}*/
 
-	@Override
-	public int[] getLocation(Cell[][] neighborhood) {
-		return neighborhood[1][1].getLocation();
-	}
+	
+	
+
 
 }
+

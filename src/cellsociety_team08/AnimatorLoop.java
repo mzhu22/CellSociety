31ac64672ddceea9940e7c6b08ecf8e5fec2155e
Brian Bolze <brianbolze@gmail.com>
2014-09-22 01:19:56 -0400

    implemented hexagonal shapes

--- src/cellsociety_team08/AnimatorLoop.java
+++ src/cellsociety_team08/AnimatorLoop.java
@@ -12,15 +12,16 @@ import javafx.util.Duration;
 
 public class AnimatorLoop {
 
-	private static final String RECTANGULAR = "Rectangular";
+	// private static final String RECTANGULAR = "Rectangular"; // Default case,
+	// so not needed
 	private static final String TRIANGULAR = "Triangular";
 	private static final String HEXAGONAL = "Hexagonal";
 
 	public static final int HEIGHT_OFFSET = 112;
 	public static final int WIDTH_OFFSET = 40;
 	public static final int GRID_BORDER_WIDTH = 1;
-	private static final double V_PAD = 0;
-	private static final double H_PAD = 0;
+	private static final double V_PAD = 5;
+	private static final double H_PAD = 5;
 	private double PADDED_WIDTH;
 	private double PADDED_HEIGHT;
 	private int HEIGHT;
@@ -53,7 +54,7 @@ public class AnimatorLoop {
 	public AnimatorLoop(int width, int height) {
 		WIDTH = width - WIDTH_OFFSET;
 		HEIGHT = height - HEIGHT_OFFSET;
-		PADDED_WIDTH = WIDTH - 2 * V_PAD;
+		PADDED_WIDTH = WIDTH - 2 * H_PAD;
 		PADDED_HEIGHT = HEIGHT - 2 * V_PAD;
 	}
 
@@ -130,32 +131,96 @@ public class AnimatorLoop {
 		return myNodes;
 	}
 
+	// EXTRACT into a ShapeBuilder Class
 	private void setShapeCoords(int row, int col, Polygon patch) {
 
 		switch (CASettings.myGridShape) {
 
-		case (TRIANGULAR):
+		case (HEXAGONAL):
 
-			double TRIANGLE_WIDTH = 2*PADDED_WIDTH / NUM_COLS;
-			double TRIANGLE_HEIGHT = PADDED_HEIGHT / NUM_ROWS;
-			if ((col+row) % 2 == 1) { //Facing down
-				col = col/2;
+			double HEX_WIDTH = PADDED_WIDTH / NUM_COLS;
+			double HEX_EDGE_SIZE = (.5)*HEX_WIDTH / Math.sqrt(3);
+			double HEX_HEIGHT = 2*HEX_EDGE_SIZE;
+			 
+			
+			if ((col + row) % 2 == 1) { // Top half of the row
+				col = col / 2;
 				patch.getPoints().setAll(
-						H_PAD + TRIANGLE_WIDTH / 2 + col * TRIANGLE_WIDTH, V_PAD + row*TRIANGLE_HEIGHT, // Left corner
-						H_PAD + TRIANGLE_WIDTH / 2 + (col + 1) * TRIANGLE_WIDTH, V_PAD + row*TRIANGLE_HEIGHT, // Right corner
-						H_PAD + (col + 1) * TRIANGLE_WIDTH, V_PAD + (row+1)*TRIANGLE_HEIGHT // Bottom corner
+						H_PAD + HEX_WIDTH * (0.5 + col),
+						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE), // Top corner
+						H_PAD + HEX_WIDTH * (col + 1),
+						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE
+								/ 2, // Top Right corner
+						H_PAD + HEX_WIDTH * (col + 1),
+						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + 3
+								* HEX_EDGE_SIZE / 2, // Bottom Right corner
+						H_PAD + HEX_WIDTH * (0.5 + col),
+						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_HEIGHT, // Bottom
+																				// corner
+						H_PAD + HEX_WIDTH * (col),
+						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + 3
+								* HEX_EDGE_SIZE / 2, // Bottom Left corner
+						H_PAD + HEX_WIDTH * (col),
+						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE
+								/ 2 // Top Left corner
 				);
-			} else { //Facing up
-				col = col/2;
+			} else {
+				col = col / 2;
 				patch.getPoints().setAll(
-						H_PAD + TRIANGLE_WIDTH / 2 + col * TRIANGLE_WIDTH, V_PAD + row*TRIANGLE_HEIGHT, // Top corner
-						H_PAD + col * TRIANGLE_WIDTH, V_PAD + (row+1)*TRIANGLE_HEIGHT, // Left corner
-						H_PAD + (col + 1) * TRIANGLE_WIDTH, V_PAD + (row+1)*TRIANGLE_HEIGHT // Right corner
+						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (0.5 + col),
+						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
+								* (HEX_HEIGHT + HEX_EDGE_SIZE), // Top corner							
+						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col + 1),
+						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
+								* (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE / 2, // Top Right Corner							
+						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col + 1),
+						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
+								* (HEX_HEIGHT + HEX_EDGE_SIZE) + 3 * HEX_EDGE_SIZE / 2, // Bottom Right Corner																				
+						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (0.5 + col),
+						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
+								* (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_HEIGHT, // Bottom Corner															
+						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col),
+						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
+								* (HEX_HEIGHT + HEX_EDGE_SIZE) + 3 * HEX_EDGE_SIZE / 2, // Bottom Left Corner																								// corner
+						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col),
+						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
+								* (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE / 2 // Top Left Corner
+																			
 				);
 			}
 			break;
 
-		default: // Rectangular
+		case (TRIANGULAR):
+
+			double TRIANGLE_WIDTH = 2 * PADDED_WIDTH / NUM_COLS;
+			double TRIANGLE_HEIGHT = PADDED_HEIGHT / NUM_ROWS;
+			if ((col + row) % 2 == 1) { // Facing down
+				col = col / 2;
+				patch.getPoints()
+						.setAll(H_PAD + TRIANGLE_WIDTH / 2 + col
+								* TRIANGLE_WIDTH,
+								V_PAD + row * TRIANGLE_HEIGHT, // Left corner
+								H_PAD + TRIANGLE_WIDTH / 2 + (col + 1)
+										* TRIANGLE_WIDTH,
+								V_PAD + row * TRIANGLE_HEIGHT, // Right corner
+								H_PAD + (col + 1) * TRIANGLE_WIDTH,
+								V_PAD + (row + 1) * TRIANGLE_HEIGHT // Bottom
+																	// corner
+						);
+			} else { // Facing up
+				col = col / 2;
+				patch.getPoints().setAll(
+						H_PAD + TRIANGLE_WIDTH / 2 + col * TRIANGLE_WIDTH,
+						V_PAD + row * TRIANGLE_HEIGHT, // Top corner
+						H_PAD + col * TRIANGLE_WIDTH,
+						V_PAD + (row + 1) * TRIANGLE_HEIGHT, // Left corner
+						H_PAD + (col + 1) * TRIANGLE_WIDTH,
+						V_PAD + (row + 1) * TRIANGLE_HEIGHT // Right corner
+				);
+			}
+			break;
+
+		default: // Rectangular!
 			double rectangleWidth = PADDED_WIDTH / NUM_COLS;
 			double rectangleHeight = PADDED_HEIGHT / NUM_ROWS;
 			patch.getPoints().setAll(

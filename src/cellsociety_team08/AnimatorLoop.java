e5ccb00d6d57193f44fab9880dfa214941cdba94
Brian Bolze <brianbolze@gmail.com>
2014-09-22 00:38:02 -0400

    Triangular + new implementation for rectangular both working

--- src/cellsociety_team08/AnimatorLoop.java
+++ src/cellsociety_team08/AnimatorLoop.java
@@ -5,18 +5,22 @@ import java.io.File;
 import javafx.animation.KeyFrame;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.layout.GridPane;
+import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class AnimatorLoop {
+
+	private static final String RECTANGULAR = "Rectangular";
+	private static final String TRIANGULAR = "Triangular";
+	private static final String HEXAGONAL = "Hexagonal";
+
 	public static final int HEIGHT_OFFSET = 112;
 	public static final int WIDTH_OFFSET = 40;
 	public static final int GRID_BORDER_WIDTH = 1;
-	private static final double V_PAD = 5;
-	private static final double H_PAD = 5;
+	private static final double V_PAD = 0;
+	private static final double H_PAD = 0;
 	private double PADDED_WIDTH;
 	private double PADDED_HEIGHT;
 	private int HEIGHT;
@@ -24,12 +28,12 @@ public class AnimatorLoop {
 	private int NUM_ROWS;
 	private int NUM_COLS;
 
-	private GridPane myGridPane;
+	private Group myNodes;
 
 	private boolean initialized = false;
 
 	private Grid myGrid;
-	private Rectangle[][] myGUICells;
+	private Polygon[][] myGUICells;
 	private Patch[][] myPatches;
 
 	/*
@@ -37,7 +41,6 @@ public class AnimatorLoop {
 	 * the size of the CA sim board. They will be used to determine the size of
 	 * each Cell in the grid.
 	 */
-
 
 	/**
 	 * Constructor
@@ -50,8 +53,8 @@ public class AnimatorLoop {
 	public AnimatorLoop(int width, int height) {
 		WIDTH = width - WIDTH_OFFSET;
 		HEIGHT = height - HEIGHT_OFFSET;
-		PADDED_WIDTH = WIDTH - 2*V_PAD;
-		PADDED_HEIGHT = HEIGHT - 2*V_PAD;
+		PADDED_WIDTH = WIDTH - 2 * V_PAD;
+		PADDED_HEIGHT = HEIGHT - 2 * V_PAD;
 	}
 
 	/**
@@ -100,87 +103,89 @@ public class AnimatorLoop {
 	/**
 	 * Create the game's scene
 	 */
-	public GridPane initGridPane(int gridRows, int gridCols, String[][] gridArray) {
+	public Group initGridPane(int gridRows, int gridCols, String[][] gridArray) {
 		NUM_ROWS = gridRows;
 		NUM_COLS = gridCols;
 
-		// Create a scene graph to organize the scene
-		//myGridPane = new Group();
-		myGridPane = new GridPane();
-		myGridPane.setVgap(GRID_BORDER_WIDTH);
-		myGridPane.setHgap(GRID_BORDER_WIDTH);
-		// Make some shapes and set their properties
+		myNodes = new Group();
 
 		addCellsToGridPane(gridArray);
 
-		return myGridPane;
+		return myNodes;
 	}
 
-	private GridPane addCellsToGridPane(String[][] gridArray) {
-		double cellHeight = HEIGHT / NUM_COLS;
-		double cellWidth = WIDTH / NUM_ROWS;
-		myGridPane.setGridLinesVisible(true);
+	private Group addCellsToGridPane(String[][] gridArray) {
+
+		for (int row = 0; row < NUM_ROWS; row++) {
+			for (int col = 0; col < NUM_COLS; col++) {
+				Polygon patch = new Polygon();
+				patch.setFill(Color.WHITE);
+				patch.setStroke(Color.GREY);
+				setShapeCoords(row, col, patch);
+				myNodes.getChildren().add(patch);
+				myGUICells[row][col] = patch;
+			}
+		}
+
+		return myNodes;
+	}
+
+	private void setShapeCoords(int row, int col, Polygon patch) {
 
 		switch (CASettings.myGridShape) {
 
-		case ("Triangular"):
-			for (int i = 0; i < NUM_ROWS; i++) {
-				for (int j = 0; j < NUM_COLS; j++) {
-					//Polygon patch = new Polygon();
-					//Code to construct points here;
-					Rectangle patch = new Rectangle(cellWidth, cellHeight,
-							Color.WHITE);
-					myGridPane.add(patch, j, i);
-					myGUICells[i][j] = patch;
-				}
+		case (TRIANGULAR):
+
+			double TRIANGLE_WIDTH = 2*PADDED_WIDTH / NUM_COLS;
+			double TRIANGLE_HEIGHT = PADDED_HEIGHT / NUM_ROWS;
+			if ((col+row) % 2 == 1) { //Facing down
+				col = col/2;
+				patch.getPoints().setAll(
+						H_PAD + TRIANGLE_WIDTH / 2 + col * TRIANGLE_WIDTH, V_PAD + row*TRIANGLE_HEIGHT, // Left corner
+						H_PAD + TRIANGLE_WIDTH / 2 + (col + 1) * TRIANGLE_WIDTH, V_PAD + row*TRIANGLE_HEIGHT, // Right corner
+						H_PAD + (col + 1) * TRIANGLE_WIDTH, V_PAD + (row+1)*TRIANGLE_HEIGHT // Bottom corner
+				);
+			} else { //Facing up
+				col = col/2;
+				patch.getPoints().setAll(
+						H_PAD + TRIANGLE_WIDTH / 2 + col * TRIANGLE_WIDTH, V_PAD + row*TRIANGLE_HEIGHT, // Top corner
+						H_PAD + col * TRIANGLE_WIDTH, V_PAD + (row+1)*TRIANGLE_HEIGHT, // Left corner
+						H_PAD + (col + 1) * TRIANGLE_WIDTH, V_PAD + (row+1)*TRIANGLE_HEIGHT // Right corner
+				);
 			}
 			break;
-		case ("Rectangular"): // different than "rectangle"
-			double rectangleWidth = PADDED_WIDTH/NUM_COLS;
-			double rectangleHeight = PADDED_HEIGHT/NUM_ROWS;
-			
-			for (int row = 0; row < NUM_ROWS; row++){
-				for (int col = 0; col < NUM_COLS; col++) {
-					Polygon patch = new Polygon();
-					patch.setFill(Color.BLUE);
-					patch.setStroke(Color.GREY);
-					patch.getPoints().setAll(
-							H_PAD + col*rectangleWidth, V_PAD + row*rectangleHeight, // Top Left corner
-							H_PAD + (col+1)*rectangleWidth, V_PAD + row*rectangleHeight, // Top Right corner
-							H_PAD + (col+1) * rectangleWidth, V_PAD + (row+1)*rectangleHeight, // Bottom Left corner
-							H_PAD + col * rectangleWidth, V_PAD + (row+1)*rectangleHeight); // Bottom Right corner
-					//myGridPane.getChildren.add(patch);
-					//myGUICells[row][col] = patch;
-				}
-			}
-			
-		default: //Rectangular
-			for (int i = 0; i < NUM_ROWS; i++) {
-				for (int j = 0; j < NUM_COLS; j++) {
-					Rectangle patch = new Rectangle(cellWidth, cellHeight,
-							Color.WHITE);
-					myGridPane.add(patch, j, i);
-					myGUICells[i][j] = patch;
-				}
-			}
+
+		default: // Rectangular
+			double rectangleWidth = PADDED_WIDTH / NUM_COLS;
+			double rectangleHeight = PADDED_HEIGHT / NUM_ROWS;
+			patch.getPoints().setAll(
+					H_PAD + col * rectangleWidth,
+					V_PAD + row * rectangleHeight, // Top Left corner
+					H_PAD + (col + 1) * rectangleWidth,
+					V_PAD + row * rectangleHeight, // Top Right corner
+					H_PAD + (col + 1) * rectangleWidth,
+					V_PAD + (row + 1) * rectangleHeight, // Bottom Left corner
+					H_PAD + col * rectangleWidth,
+					V_PAD + (row + 1) * rectangleHeight // Bottom Right corner
+			);
 			break;
+
 		}
 
-		return myGridPane;
 	}
 
-	public GridPane readXMLAndInitializeGrid(File XMLFile) {
+	public Group readXMLAndInitializeGrid(File XMLFile) {
 		XMLReader reader = new XMLReader();
 		CASettings settings = reader.read(XMLFile);
-		myGUICells = new Rectangle[settings.getRows()][settings.getColumns()];
-		//myGUICells = new Polygon[settings.getRows()][settings.getColumns()];
+		myGUICells = new Polygon[settings.getRows()][settings.getColumns()];
+		// myGUICells = new Polygon[settings.getRows()][settings.getColumns()];
 
-		myGridPane = initGridPane(settings.getRows(), settings.getColumns(),
+		myNodes = initGridPane(settings.getRows(), settings.getColumns(),
 				settings.getGrid());
 		myGrid = new Grid(settings.getType(), settings.getParameters(),
 				settings.getRows(), settings.getColumns(), settings.getGrid());
 		initialized = true;
-		return myGridPane;
+		return myNodes;
 	}
 
 }

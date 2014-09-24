65d502a1d319fbe4d7e7837578f92db127009f15
Brian Bolze <brianbolze@gmail.com>
2014-09-24 11:17:50 -0400

    Very minor cleanup in AnimatorLoop

--- src/cellsociety_team08/AnimatorLoop.java
+++ src/cellsociety_team08/AnimatorLoop.java
@@ -13,23 +13,16 @@ import javafx.animation.KeyFrame;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.util.Duration;
 
 public class AnimatorLoop {
 
-	// private static final String RECTANGULAR = "Rectangular"; // Default case,
-	// so not needed
-	private static final String TRIANGULAR = "Triangular";
-	private static final String HEXAGONAL = "Hexagonal";
-
 	public static final int HEIGHT_OFFSET = 112;
 	public static final int WIDTH_OFFSET = 40;
 	public static final int GRID_BORDER_WIDTH = 1;
-	
-	private GridPane myGridPane;
+
 	private CASettings mySettings;
 	
 	private static final double V_PAD = 5;
@@ -161,114 +154,6 @@ public class AnimatorLoop {
 //		myGridPane = initGrid(mySettings.getRows(), mySettings.getColumns(), mySettings.getGrid());
 //		myGrid = new Grid(mySettings.getType(), mySettings.getParameters(), mySettings.getRows(), mySettings.getColumns(), mySettings.getGrid());
 //	}
-	
-	// EXTRACT into a ShapeBuilder Class
-	private void setShapeCoords(int row, int col, Polygon patch) {
-
-		switch (CASettings.myGridShape) {
-
-		case (HEXAGONAL):
-
-			double HEX_WIDTH = PADDED_WIDTH / NUM_COLS;
-			double HEX_EDGE_SIZE = (.5)*HEX_WIDTH / Math.sqrt(3);
-			double HEX_HEIGHT = 2*HEX_EDGE_SIZE;
-			 
-			
-			if ((col + row) % 2 == 1) { // Top half of the row
-				col = col / 2;
-				patch.getPoints().setAll(
-						H_PAD + HEX_WIDTH * (0.5 + col),
-						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE), // Top corner
-						H_PAD + HEX_WIDTH * (col + 1),
-						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE
-								/ 2, // Top Right corner
-						H_PAD + HEX_WIDTH * (col + 1),
-						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + 3
-								* HEX_EDGE_SIZE / 2, // Bottom Right corner
-						H_PAD + HEX_WIDTH * (0.5 + col),
-						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_HEIGHT, // Bottom
-																				// corner
-						H_PAD + HEX_WIDTH * (col),
-						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + 3
-								* HEX_EDGE_SIZE / 2, // Bottom Left corner
-						H_PAD + HEX_WIDTH * (col),
-						V_PAD + (row) * (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE
-								/ 2 // Top Left corner
-				);
-			} else {
-				col = col / 2;
-				patch.getPoints().setAll(
-						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (0.5 + col),
-						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
-								* (HEX_HEIGHT + HEX_EDGE_SIZE), // Top corner							
-						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col + 1),
-						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
-								* (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE / 2, // Top Right Corner							
-						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col + 1),
-						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
-								* (HEX_HEIGHT + HEX_EDGE_SIZE) + 3 * HEX_EDGE_SIZE / 2, // Bottom Right Corner																				
-						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (0.5 + col),
-						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
-								* (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_HEIGHT, // Bottom Corner															
-						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col),
-						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
-								* (HEX_HEIGHT + HEX_EDGE_SIZE) + 3 * HEX_EDGE_SIZE / 2, // Bottom Left Corner																								// corner
-						H_PAD + HEX_EDGE_SIZE + HEX_WIDTH * (col),
-						3 * HEX_EDGE_SIZE / 2 + V_PAD + (row)
-								* (HEX_HEIGHT + HEX_EDGE_SIZE) + HEX_EDGE_SIZE / 2 // Top Left Corner
-																			
-				);
-			}
-			break;
-
-		case (TRIANGULAR):
-
-			double TRIANGLE_WIDTH = 2 * PADDED_WIDTH / NUM_COLS;
-			double TRIANGLE_HEIGHT = PADDED_HEIGHT / NUM_ROWS;
-			if ((col + row) % 2 == 1) { // Facing down
-				col = col / 2;
-				patch.getPoints()
-						.setAll(H_PAD + TRIANGLE_WIDTH / 2 + col
-								* TRIANGLE_WIDTH,
-								V_PAD + row * TRIANGLE_HEIGHT, // Left corner
-								H_PAD + TRIANGLE_WIDTH / 2 + (col + 1)
-										* TRIANGLE_WIDTH,
-								V_PAD + row * TRIANGLE_HEIGHT, // Right corner
-								H_PAD + (col + 1) * TRIANGLE_WIDTH,
-								V_PAD + (row + 1) * TRIANGLE_HEIGHT // Bottom
-																	// corner
-						);
-			} else { // Facing up
-				col = col / 2;
-				patch.getPoints().setAll(
-						H_PAD + TRIANGLE_WIDTH / 2 + col * TRIANGLE_WIDTH,
-						V_PAD + row * TRIANGLE_HEIGHT, // Top corner
-						H_PAD + col * TRIANGLE_WIDTH,
-						V_PAD + (row + 1) * TRIANGLE_HEIGHT, // Left corner
-						H_PAD + (col + 1) * TRIANGLE_WIDTH,
-						V_PAD + (row + 1) * TRIANGLE_HEIGHT // Right corner
-				);
-			}
-			break;
-
-		default: // Rectangular!
-			double rectangleWidth = PADDED_WIDTH / NUM_COLS;
-			double rectangleHeight = PADDED_HEIGHT / NUM_ROWS;
-			patch.getPoints().setAll(
-					H_PAD + col * rectangleWidth,
-					V_PAD + row * rectangleHeight, // Top Left corner
-					H_PAD + (col + 1) * rectangleWidth,
-					V_PAD + row * rectangleHeight, // Top Right corner
-					H_PAD + (col + 1) * rectangleWidth,
-					V_PAD + (row + 1) * rectangleHeight, // Bottom Left corner
-					H_PAD + col * rectangleWidth,
-					V_PAD + (row + 1) * rectangleHeight // Bottom Right corner
-			);
-			break;
-
-		}
-
-	}
 
 	public Group readXMLAndInitializeGrid(File XMLFile) {
 		XMLHandler reader = new XMLHandler();
